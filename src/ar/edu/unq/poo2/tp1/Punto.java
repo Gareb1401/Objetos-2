9a7edda39099c0fbcfa02b6b60d5a8b98388f28a
Raimondi, Gaston <gaston14015@gmail.com>
2025-08-15 19:10:18 -0300

    Se crearon los getters y los setters

--- src/ar/edu/unq/poo2/tp1/Punto.java
+++ src/ar/edu/unq/poo2/tp1/Punto.java
@@ -1,5 +1,18 @@
 package ar.edu.unq.poo2.tp1;
 
 public class Punto {
-	
+	private float x;
+	private float y;
+	public float getX() {
+		return x;
+	}
+	public void setX(float x) {
+		this.x = x;
+	}
+	public float getY() {
+		return y;
+	}
+	public void setY(float y) {
+		this.y = y;
+	}
 }
